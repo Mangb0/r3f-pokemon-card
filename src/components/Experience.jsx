b474ca4c8b951cbc54e60cb77fe5534193e560b8
Mangb0 <whdudghks45629@gmail.com>
2023-12-03 22:35:17 +0900

    MeshPortalMaterial

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -1,4 +1,10 @@
-import { Environment, OrbitControls, useTexture } from "@react-three/drei";
+import {
+  Environment,
+  MeshPortalMaterial,
+  OrbitControls,
+  RoundedBox,
+  useTexture,
+} from "@react-three/drei";
 import * as THREE from "three";
 import { Fish } from "./Fish";
 
@@ -12,11 +18,17 @@ export const Experience = () => {
       <ambientLight intensity={0.5} />
       <Environment preset="sunset" />
       <OrbitControls />
-      <Fish scale={0.6} position-y={-1} />
-      <mesh>
-        <sphereGeometry args={[5, 64, 64]} />
-        <meshStandardMaterial map={map} side={THREE.BackSide} />
-      </mesh>
+      <RoundedBox args={[2, 3, 0.1]}>
+        <MeshPortalMaterial side={THREE.DoubleSide}>
+          <ambientLight intensity={1} />
+          <Environment preset="sunset" />
+          <Fish scale={0.6} position-y={-1} />
+          <mesh>
+            <sphereGeometry args={[5, 64, 64]} />
+            <meshStandardMaterial map={map} side={THREE.BackSide} />
+          </mesh>
+        </MeshPortalMaterial>
+      </RoundedBox>
     </>
   );
 };
