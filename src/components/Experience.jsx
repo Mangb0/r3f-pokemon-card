1bc069b835ff9e26555edb7cf4e86b04ea107f40
Mangb0 <whdudghks45629@gmail.com>
2023-12-08 23:59:48 +0900

    Enter into worlds

--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -10,8 +10,12 @@ import * as THREE from "three";
 import { Fish } from "./Fish";
 import { DragonEvolved } from "./Dragon_Evolved";
 import { Cactoro } from "./Cactoro";
+import { useRef, useState } from "react";
+import { useFrame } from "@react-three/fiber";
+import { easing } from "maath";
 
 export const Experience = () => {
+  const [active, setActive] = useState(null);
   return (
     <>
       <ambientLight intensity={0.5} />
@@ -23,6 +27,8 @@ export const Experience = () => {
         }
         name="Fish King"
         color="#38adcf"
+        active={active}
+        setActive={setActive}
       >
         <Fish scale={0.6} position-y={-1} />
       </MonsterStage>
@@ -32,6 +38,8 @@ export const Experience = () => {
         color="#df8d52"
         position-x={-2.5}
         rotation-y={Math.PI / 8}
+        active={active}
+        setActive={setActive}
       >
         <DragonEvolved scale={0.5} position-y={-1} />
       </MonsterStage>
@@ -41,6 +49,8 @@ export const Experience = () => {
         color="#739d3c"
         position-x={2.5}
         rotation-y={-Math.PI / 8}
+        active={active}
+        setActive={setActive}
       >
         <Cactoro scale={0.45} position-y={-1} />
       </MonsterStage>
@@ -48,8 +58,23 @@ export const Experience = () => {
   );
 };
 
-const MonsterStage = ({ children, texture, name, color, ...props }) => {
+const MonsterStage = ({
+  children,
+  texture,
+  name,
+  color,
+  active,
+  setActive,
+  ...props
+}) => {
   const map = useTexture(texture);
+  const portalMaterial = useRef();
+
+  useFrame((_state, delta) => {
+    const worldOpen = active === name;
+    easing.damp(portalMaterial.current, "blend", worldOpen ? 1 : 0, 0.2, delta);
+  });
+
   return (
     <group {...props}>
       <Text
@@ -61,8 +86,11 @@ const MonsterStage = ({ children, texture, name, color, ...props }) => {
         {name}
         <meshBasicMaterial color={color} toneMapped={false} />
       </Text>
-      <RoundedBox args={[2, 3, 0.1]}>
-        <MeshPortalMaterial side={THREE.DoubleSide}>
+      <RoundedBox
+        args={[2, 3, 0.1]}
+        onDoubleClick={() => setActive(active === name ? null : name)}
+      >
+        <MeshPortalMaterial ref={portalMaterial} side={THREE.DoubleSide}>
           <ambientLight intensity={1} />
           <Environment preset="sunset" />
           {children}
